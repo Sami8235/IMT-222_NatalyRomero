996031f1557694b159522388294058cc95506186
Sami8235 <nataly.romero@ucb.edu.bo>
2025-08-25 17:41:12 -0400

    Agrego Practica_2 con funciones.cpp y funciones.h

---
+++ Practica_2/Practica_2.ino
@@ -0,0 +1,12 @@
+
+#include "funciones.h"
+void setup() {
+  // put your setup code here, to run once:
+  configuracion();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  espera();
+  secuencia();
+}
---
+++ Practica_2/funciones.h
@@ -0,0 +1,16 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+#include <Arduino.h>
+#define DELAY 1000
+#define LED1 2
+#define LED2 3
+#define LED3 4
+#define LED4 5
+#define BTN 6
+#define BAUDRATE 9600 //CLCK DEL SERIAL:protocolo de configuracion asincrono
+
+//uint8_t flag=0;
+void configuracion();
+void espera();
+void secuencia();
+#endif
